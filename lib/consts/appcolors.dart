2a0358ed01a462ec8c174bc4cbd5574bf63d2d13
Ziad Omran <ziadomran814@gmail.com>
2026-09-06 17:17:01 +0300

    structur of project

---
+++ lib/consts/appcolors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFF5669FF);
+  static const colorsecondry = Color(0xFFF2FEFF);
+  static const Color grayColor = Color(0xFF7B7B7B);
+  static const Color textdarkcolor = Color(0xFFF4EBDC);
+  static const Color textligthcolor = Color(0xFF1C1C1C);
+  
+  static const Color redColor = Color(0xFFFF5659);
+  static const Color darkprimary = Color(0xFF101127);
+}
---
+++ lib/consts/appthem.dart
@@ -0,0 +1,81 @@
+import 'package:envently/consts/appcolors.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppTheme {
+  ThemeData lightTheme = ThemeData(
+    textTheme: _gettextThem(AppColors.textligthcolor),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.primaryColor,
+    ),
+    scaffoldBackgroundColor: AppColors.colorsecondry,
+    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      backgroundColor: AppColors.colorsecondry,
+      titleTextStyle: TextStyle(
+        color: AppColors.primaryColor,
+        fontSize: 22,
+        fontWeight: FontWeight.w600,
+      ),
+      iconTheme: IconThemeData(color: AppColors.primaryColor),
+    ),
+  );
+  ThemeData darkTheme = ThemeData(
+    textTheme: _gettextThem(AppColors.textdarkcolor),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.darkprimary,
+    ),
+    scaffoldBackgroundColor: AppColors.darkprimary,
+    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.darkprimary),
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      backgroundColor: AppColors.darkprimary,
+      titleTextStyle: TextStyle(
+        color: AppColors.primaryColor,
+        fontSize: 22,
+        fontWeight: FontWeight.w600,
+      ),
+      iconTheme: IconThemeData(color: AppColors.primaryColor),
+    ),
+  );
+
+  static TextTheme _gettextThem(Color textcolor) {
+    return TextTheme(
+      labelSmall: TextStyle(
+        color: textcolor,
+        fontSize: 10,
+        fontWeight: FontWeight.w500,
+      ),
+      labelLarge: TextStyle(
+        color: textcolor,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      bodySmall: TextStyle(
+        color: textcolor,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: TextStyle(
+        color: textcolor,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyLarge: TextStyle(
+        color: textcolor,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      titleSmall: TextStyle(
+        color: textcolor,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      titleMedium: TextStyle(
+        color: textcolor,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+}
